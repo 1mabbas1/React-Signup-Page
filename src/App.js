f91eb26e787ce95a37bc4ed7de198334699570c0
Mohammad Abbas <10mabbas@googlemail.com>
2021-12-21 11:44:01 +0000

    added ability to store users and display details

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import AddUser from "./Components/AddUser/AddUser";
+import UsersList from "./Components/AddUser/UsersList";
 
 function App() {
+  const [usersList, setusersList] = useState([]);
+
+  function addUserHandler(username, email) {
+    setusersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        { id: Math.random().toString(), username: username, email: email },
+      ];
+    });
+  }
+
   return (
     <div>
-      <AddUser />
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../UI/Card";
 import styles from "./AddUser.module.css";
 import Button from "../UI/Button";
 
 const AddUser = (props) => {
+  const [username, setUsername] = new useState("");
+  const [email, setEmail] = new useState("");
+
   const submitHandler = (event) => {
+    if (username.trim() === 0 || email.trim() === 0) {
+      return;
+    }
     event.preventDefault();
+    props.onAddUser(username, email);
+    setUsername("");
+    setEmail("");
+  };
+
+  const newUsername = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const newEmail = (event) => {
+    setEmail(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form onSubmit={submitHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text" />
+        <input
+          onChange={newUsername}
+          value={username}
+          id="username"
+          type="text"
+        />
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" />
+        <input onChange={newEmail} value={email} id="email" type="email" />
         <Button type="submit">Submit</Button>
       </form>
     </Card>
